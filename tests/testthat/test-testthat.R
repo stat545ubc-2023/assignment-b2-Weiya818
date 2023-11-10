a2e3724bfee2a4f500f6bcb4563a725c6597ca4e
Weiya Zhu <weiyazhu@student.ubc.ca>
2023-11-09 18:02:16 -0800

    finalize test file

--- tests/testthat/test-testthat.R
+++ tests/testthat/test-testthat.R
@@ -1,15 +1,12 @@
-library(ggplot2)
-library(datateachr)
-
 test_that("Test 1: Function can only have continuous numerical x values", {
-  expect_error(density_plot(cancer_sample, cancer_sample$diagnosis, cancer_sample$diagnosis, x_axis_name="Diagnosis"))
+  expect_error(density_plot(datateachr::cancer_sample, datateachr::cancer_sample$diagnosis, datateachr::cancer_sample$diagnosis, x_axis_name="Diagnosis"))
 })
 
 test_that("Test 2: Function only takes four input variable", {
-  expect_error(density_plot(cancer_sample, cancer_sample$radius_mean, cancer_sample$diagnosis, cancer_sample$diagnosis, x_axis_name="Radius_Mean"))
+  expect_error(density_plot(datateachr::cancer_sample, datateachr::cancer_sample$radius_mean, datateachr::cancer_sample$diagnosis, datateachr::cancer_sample$diagnosis, x_axis_name="Radius_Mean"))
 })
 
 test_that("Test 3: Function can only have categorical fill values", {
-  expect_error(density_plot( data=cancer_sample, x=cancer_sample$radius_mean, fill=cancer_sample$area_mean, x_axis_name="Radius_Mean"))
+  expect_error(density_plot( datateachr::cancer_sample, datateachr::cancer_sample$radius_mean, datateachr::cancer_sample$area_mean, x_axis_name="Radius_Mean"))
 })
 
