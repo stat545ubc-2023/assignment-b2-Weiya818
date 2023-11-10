b80b3e486568d953d370f49b60640a04f9d5b825
Weiya Zhu <weiyazhu@student.ubc.ca>
2023-11-09 18:07:35 -0800

    Fixed typo in R file

--- R/density_plot.R
+++ R/density_plot.R
@@ -1,20 +1,19 @@
 #' density_plot
-#' @description The density_plot function creates a density plot wiht alpha=0.5
+#' @description The density_plot function creates a density plot with alpha=0.5
 #' based on the given inputs, dataset, x, and fill values, using ggplot2.
 #' This function can be used as a visualization tool to represent the
 #' distribution of a continuous numeric variable (x) across multiple fills(categorical values).
 #'
 #' @param data The input dataset
-#' @param x A continuous numerical variable in the dataset, it is one of the variable
-#'being investigated
-#' @param fill A variable in the dataset, it is one of the variable being investigated
+#' @param x A continuous numerical variable in the dataset, it is one of the variables being investigated
+#' @param fill A variable in the dataset, it is one of the variables being investigated
 #' @param x_axis_name This is the name of the x-axis given by the user who uses this function
 #'
 #'
 #'@examples density_plot(data = datateachr::cancer_sample, x = datateachr::cancer_sample$radius_mean,
 #'fill = datateachr::cancer_sample$diagnosis, x_axis_name = "radius_mean")
 #'
-#' @return A density plot with value of x in x-axis across different fills and density as the y-axis
+#' @return A density plot with the value of x in x-axis across different fills and density as the y-axis
 #' @export
 
 density_plot <- function(data, x, fill,x_axis_name) {
