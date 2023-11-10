c1dea2e36644aa47a5dfa8f2a7f85f723b29c480
Weiya Zhu <weiyazhu@student.ubc.ca>
2023-11-09 17:59:05 -0800

    Re-edit the test file, all pass

--- tests/testthat/test-testthat.R
+++ tests/testthat/test-testthat.R
@@ -1,20 +1,15 @@
 library(ggplot2)
 library(datateachr)
 
-test_that("Test 1: Function creates a ggplot with a custom x-axis name", {
-  expected_val <- density_plot(cancer_sample, cancer_sample$radius_mean, cancer_sample$diagnosis,"Radius_Mean")
-  expect_is(expected_val, "ggplot")
-})
-
-test_that("Test 2: Function can only have continuous numerical x values", {
+test_that("Test 1: Function can only have continuous numerical x values", {
   expect_error(density_plot(cancer_sample, cancer_sample$diagnosis, cancer_sample$diagnosis, x_axis_name="Diagnosis"))
 })
 
-test_that("Test 3: Function only takes four input variable", {
+test_that("Test 2: Function only takes four input variable", {
   expect_error(density_plot(cancer_sample, cancer_sample$radius_mean, cancer_sample$diagnosis, cancer_sample$diagnosis, x_axis_name="Radius_Mean"))
 })
 
-test_that("Test 4: Function can only have categorical fill values", {
+test_that("Test 3: Function can only have categorical fill values", {
   expect_error(density_plot( data=cancer_sample, x=cancer_sample$radius_mean, fill=cancer_sample$area_mean, x_axis_name="Radius_Mean"))
 })
 
